74c3c313eabc8f9ee5af6f060be7abfc71f0d4f4
Alexis Zugasti <alexiszugasti@gmail.com>
2022-07-22 07:50:47 -0500

    Added book get function, added some api functions

--- book_inventory/index.js
+++ book_inventory/index.js
@@ -3,6 +3,66 @@ const app = express();
 
 const PORT = 4000 
 
-app.listen(PORT, () => {
-    console.log(`Server on port ${PORT}`)
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+var books = [
+    {
+        id: 1,
+        title: "Project Hail Mary",
+        author: "Andy Weir",
+        editorial: "Penguin",
+        genre: "Sci-Fi"
+    },
+    {
+        id: 2,
+        title: "The Silmarillion",
+        author: "J.R.R. Tolkien",
+        editorial: "Harper Collins",
+        genre: "Fantasy"
+    },
+    {
+        id: 3,
+        title: "Later",
+        author: "Stephen King",
+        editorial: "Titan Books",
+        genre: "Thriller"
+    },
+]
+
+
+//API Welcome call
+app.get("/",(rec,res) =>{
+    res.send("Welcome to the book API")
 })
+
+//Returns more info about API
+app.get("/about",(rec,res) =>{
+    res.send("Aqui Se enviara mas informacion sobre la API")
+})
+
+//Return a list of all current books
+app.get("/books", function(req, res){
+    return res.json(books);
+})
+
+app.post("/books", function(req,res){
+    res.send("Add book function")
+})
+
+//Return info about specific book based on id, if not available return error message
+app.get("/books/:id", function(req, res){
+    let bookId = req.params.id;
+    for(let i =0; i< books.length; i++){
+        if(bookId == books[i].id){
+            return res.json(books[i])
+        }
+    }
+    return res.send(`No info about book ${bookId}`)
+})
+
+//Listen function for the API
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+  })
+
